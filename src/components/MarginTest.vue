d9d81b3c8b95ade281adade9ea31a35b8167e9fe
perry <i-perhuang@malong.com>
2018-12-18 21:04:57 +0800

    [misc]正在学习margin

--- src/components/MarginTest.vue
+++ src/components/MarginTest.vue
@@ -27,15 +27,39 @@
         <p>观点2</p>
       </div>
     </div>
-    <div>
-      adjkasdjka
+    <!--在元素的尺寸表现为充分利用可利用的空间时，使用margin是可以改变元素的大小的
+    下面内部的p元素因为为块级元素，所以充分利用可利用可用空间，
+    这时只要其左右margin设为负值便会使p元素变大，垂直方向只有绝对定位才会出现这种情况-->
+    <div style="width:400px;height:200px;background-color:red;
+      margin-top:100px;margin-left: 100px">
+      <p style="height: 100px;margin: 0 -50px;background-color: rgba(0,128,0,0.4)"></p>
     </div>
+    <div style="width: 300px;height:200px;background-color: yellow;position: relative">
+      <p style="position: absolute;left: 0;right: 0;top: 0;
+        bottom: 0;background-color: rgba(0,0,255,0.25);margin-top: -50px"></p>
+    </div>
+    <!--利用margin消除ul li的最后一个margin,ul必须满足充分利用可利用空间的条件，否则无法改变内部尺寸，即无法消除-->
+    <div class="mTest-ul">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+    <p></p>
+    <span style="background-color: red;margin-bottom: 100px;height: 100px;">12312312312</span>
+    <div style="background-color: blue;margin-top: 100px;height: 100px;"></div>
+    <span style="position: absolute" class="float-span">浮动的元素其display变为block</span>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
-    name: 'MarginTest'
+    name: 'MarginTest',
+    mounted() {
+      const span = document.querySelector('.float-span');
+      console.log('值：', window.getComputedStyle(span).display);
+    }
   };
 </script>
 
@@ -87,6 +111,7 @@
 
     &-equalHeight {
       overflow: hidden;
+
       &::after {
         content: '';
         display: table;
@@ -106,6 +131,23 @@
 
       &-right {
         background-color: blue;
+      }
+    }
+    &-ul {
+      margin-top: 50px;
+      margin-bottom: 50px;
+      width: 700px;
+      display: inline-block;
+      ul {
+        background-color: black;
+        margin-right: -50px;
+        li {
+          width: 100px;
+          height: 200px;
+          background-color: green;
+          display: inline-block;
+          margin-right: 50px;
+        }
       }
     }
   }
